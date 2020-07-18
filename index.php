beaabd72a560da33268ceb2386c06e08509384f4
Johan Castillo <jcjohan2707@gmail.com>
2020-07-18 01:32:41 -0400

    Se añadieron cambios

--- index.php
+++ index.php
@@ -4,9 +4,14 @@ require __DIR__.'/vendor/autoload.php';
 
 use Spipu\Html2Pdf\Html2Pdf;
 
-$html2pdf = new Html2Pdf();
-$html2pdf->writeHTML('<h1>Hola Mundo</h1>Con Html2Pdf');
-$html2pdf->output();
+//Import file "view.php"
+ob_start();
+require_once 'view.php';
+$html = ob_get_clean();
+
+$html2pdf = new Html2Pdf('P','A4','es','true','UTF-8');
+$html2pdf->writeHTML($html);
+$html2pdf->output("calculos-hipotecarios.pdf");
 
 
 
---
+++ view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Resultado PDF</title>
+
+    <style type="text/css">
+      
+    </style>
+  </head>
+  <body>
+    <h1>Hola Mundo</h1>
+  </body>
+</html>
